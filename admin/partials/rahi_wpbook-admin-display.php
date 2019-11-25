451aa73d8f8847917909c09bfa8c1d161ee6adfa
Tan-007 <rahi.prajapati1811@gmail.com>
2019-11-26 01:23:30 +0530

    fixed metabox number type not working

--- admin/partials/rahi_wpbook-admin-display.php
+++ admin/partials/rahi_wpbook-admin-display.php
@@ -3,7 +3,7 @@
 /**
  * Provide a admin area view for the plugin
  *
- * This file is used to markup the admin-facing aspects of the plugin.
+ * This file is used to markup the admin-facing aspects of the plugin
  *
  * @link       https://rahicodes.wordpress.com
  * @since      1.0.0
